6396f69d5a07fa8d26404cfbe05b23686b71e98d
sid597 <siddharthdv77@gmail.com>
2020-04-24 20:12:08 +0530

    Project 4 multiplication implementation done

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,49 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+ 
+    @R0    // set a to r0
+    D=M
+    @a
+    M=D
+
+    @R1    // set b to r1
+    D=M
+    @b
+    M=D
+
+    @sum   // set sum to 0
+    M=0   
+
+    (LOOP)
+
+    @a     // get value of a
+    D=M
+
+    @STOP  // If d less than 0 stop
+    D;JEQ
+
+    @b     // Get value of b to add to sum
+    D=M
+
+    @sum
+    M=M+D
+
+    @a     // Subtract 1 from a
+    M=M-1
+
+    @LOOP
+    0;JMP
+
+
+    (STOP)
+    @sum
+    D=M
+    @R2
+    M=D
+
+    (END)
+    @END
+    0;JMP
+
+    
